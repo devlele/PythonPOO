070a1edbebc8c1e80f1a182d089a207eaf748dd0
Alefe Freitas Santos <alefe.santos@afinz.com.br>
2024-12-05 19:45:33 -0300

    Primeira classe

---
+++ modelos/restaurante.py
@@ -0,0 +1,9 @@
+class Restaurante:
+    Nome = ''
+    Categoria = ''
+    Status = False
+
+restaurante_praca = Restaurante()
+restaurante_praca.Nome = 'Praça'
+restaurante_praca.Categoria = 'Gourmet'
+
