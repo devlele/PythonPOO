ff28ff4f8558383671f2d203b00cd1d8e2363319
Alefe Freitas Santos <alefe.santos@afinz.com.br>
2024-12-06 19:21:17 -0300

    Criando e trabalhando com metodos

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,9 +1,25 @@
 class Restaurante:
-    Nome = ''
-    Categoria = ''
-    Status = False
+    restaurantes = []
 
-restaurante_praca = Restaurante()
-restaurante_praca.Nome = 'Praça'
-restaurante_praca.Categoria = 'Gourmet'
+    def __init__(self, Nome, Categoria):
+        self.Nome = Nome
+        self.Categoria = Categoria
+        self.Status = False
+
+        Restaurante.restaurantes.append(self)
+    def __str__(self):
+        return f'{self.Nome} | {self.Categoria}'
+        
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.Nome}|{restaurante.Categoria}|{restaurante.Status}')
+
+    @property
+    def Status(self):
+        return '1' if self.status else '0'
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza', 'Italiana')
+
+Restaurante.listar_restaurantes()
 
