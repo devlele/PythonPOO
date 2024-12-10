cb7f71a0bee1e205fcba2e5b93f8f3b9b7bd1035
Alefe Freitas Santos <alefe.santos@afinz.com.br>
2024-12-10 19:25:59 -0300

    Utiliazndo property

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,22 +1,22 @@
 class Restaurante:
     restaurantes = []
 
-    def __init__(self, Nome, Categoria):
-        self.Nome = Nome
-        self.Categoria = Categoria
-        self.Status = False
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self._status = False # o underline transforma esse atributo em um atributo privado (apenas em convenção então ele ainda é acessivel), ou seja, não é esperado que oo usuario mude o vlaor dele
 
         Restaurante.restaurantes.append(self)
     def __str__(self):
-        return f'{self.Nome} | {self.Categoria}'
+        return f'{self.nome} | {self.categoria}'
         
     def listar_restaurantes():
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.Nome}|{restaurante.Categoria}|{restaurante.Status}')
+            print(f'{restaurante.nome.ljust(25)}|{restaurante.categoria.ljust(25)}|{restaurante.status}')
 
     @property
-    def Status(self):
-        return '1' if self.status else '0'
+    def status(self):
+        return '1' if self._status else '0'
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza', 'Italiana')
