518c0e04b341134421693e592e108687c7464f43
Alefe Freitas Santos <alefe.santos@afinz.com.br>
2024-12-12 14:52:30 -0300

    from e import

---
+++ app.py
@@ -0,0 +1,16 @@
+from modelos.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.receber_avaliacao(Guilherme, 10)
+restaurante_praca.receber_avaliacao(Talita, 5)
+restaurante_praca.receber_avaliacao(João, 8)
+restaurante_praca.receber_avaliacao(Carol, 7)
+
+
+
+def main():
+    Restaurante.listar_restaurantes()
+
+
+if __name__ == '__main__':
+    main()
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,25 +1,44 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self.categoria = categoria.upper()
         self._status = False # o underline transforma esse atributo em um atributo privado (apenas em convenção então ele ainda é acessivel), ou seja, não é esperado que oo usuario mude o vlaor dele
+        self._avaliacao = []
 
         Restaurante.restaurantes.append(self)
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
-        
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)}|{restaurante.categoria.ljust(25)}|{restaurante.status}')
+        return f'{self._nome} | {self.categoria}'
+    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)}|{restaurante.categoria.ljust(25)}|{restaurante._status}')
 
     @property
     def status(self):
         return '1' if self._status else '0'
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza', 'Italiana')
+    def alternar_estado(self):
+        self._status = not self._status
 
-Restaurante.listar_restaurantes()
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
 
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 0
+        
+        soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        
+        quantidade_notas = len(self._avaliacao)
+
+        media = round(soma_notas / quantidade_notas, 1)
+        return media
